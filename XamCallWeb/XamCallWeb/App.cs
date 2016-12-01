620058f9d5007367923b3854566f698f49c30234
filipedgb <filipedgb@gmail.com>
2016-12-01 19:59:37 +0000

    Default project

---
+++ XamCallWeb/XamCallWeb/App.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamCallWeb {
+  public class App : Application {
+    public App() {
+      // The root page of your application
+      var content = new CallPage {
+        Title = "XamCallWeb",
+      };
+      MainPage = new NavigationPage(content);
+    }
+
+    protected override void OnStart() {
+      // Handle when your app starts
+    }
+
+    protected override void OnSleep() {
+      // Handle when your app sleeps
+    }
+
+    protected override void OnResume() {
+      // Handle when your app resumes
+    }
+  }
+}
